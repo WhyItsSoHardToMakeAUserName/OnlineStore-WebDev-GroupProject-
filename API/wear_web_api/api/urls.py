441b9e0db5654908303d4e751da42f21c2f25c5b
Askar Oralkhan <askar4ik123@gmail.com>
2024-04-23 17:29:06 +0500

    added CRUD functionality on category entity

--- API/wear_web_api/api/urls.py
+++ API/wear_web_api/api/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import category_list
+from .views_category import category_list, category_details
 
 urlpatterns = [
     path('categories/', category_list),
+    path('categories/<int:id>', category_details),
 ]
