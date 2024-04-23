3dbf0b200f2d7d808e464bf4b6ea2918bed6bfa3
Askar Oralkhan <askar4ik123@gmail.com>
2024-04-23 18:28:58 +0500

    added CRUD functionality to hat entity

--- API/wear_web_api/api/serializers.py
+++ API/wear_web_api/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Category, Size
+from .models import Category, Size, Hat, Top, Pant, Shoe
 
 class CategorySerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only = True)
@@ -24,4 +24,24 @@ class SizeSerializer(serializers.Serializer):
         instance.size = validated_data.get('size', instance.size)
         instance.save()
         return instance
+
+class HatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hat
+        fields = '__all__'
+
+class TopSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Top
+        field = ('name', 'brand', 'price', 'image_url', 'gender', 'category', 'sizes')
+        
+class PantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pant
+        field = ('name', 'brand', 'price', 'image_url', 'gender', 'category', 'sizes')
+
+class ShoeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Shoe
+        field = ('name', 'brand', 'price', 'image_url', 'gender', 'category', 'sizes')
 
--- API/wear_web_api/api/urls.py
+++ API/wear_web_api/api/urls.py
@@ -2,11 +2,15 @@ from django.contrib import admin
 from django.urls import path
 
 from .views_category import category_list, category_details
-from.views_size import size_list, size_details
+from .views_size import size_list, size_details
+from .views_hat import HatCRUD, HatsCRUD
 
 urlpatterns = [
     path('categories/', category_list),
     path('categories/<int:id>', category_details),
     path('sizes/', size_list),
     path('sizes/<int:id>', size_details),
+
+    path('hats/', HatsCRUD.as_view()),
+    path('hats/<int:id>/', HatCRUD.as_view()),
 ]
