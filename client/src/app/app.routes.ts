49535d3e3c1d3d4e7c428eee72fe2c8d32c63ff7
WhyItsSoHardToMakeAUserName <86366420+WhyItsSoHardToMakeAUserName@users.noreply.github.com>
2024-04-25 10:54:21 +0500

    Shopping cart demo

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { ProductPageComponent } from './product-page/product-page.component';
+import { ShoppingCartPageComponent } from './shopping-cart-page/shopping-cart-page.component';
 
 export const routes: Routes = [
     {path: '',component: MainPageComponent},
-    {path:'test',component:ProductPageComponent}
+    {path:'test',component:ProductPageComponent},
+    {path:'cart',component:ShoppingCartPageComponent}
 ];
