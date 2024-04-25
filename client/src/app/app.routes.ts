8b165a5de231c5ea44d5793a7fda5b992791af10
WhyItsSoHardToMakeAUserName <86366420+WhyItsSoHardToMakeAUserName@users.noreply.github.com>
2024-04-25 12:04:55 +0500

    Added James's Login Page

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ShoppingCartPageComponent } from './shopping-cart-page/shopping-cart-page.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     {path: '',component: MainPageComponent},
     {path:'test',component:ProductPageComponent},
-    {path:'cart',component:ShoppingCartPageComponent}
+    {path:'cart',component:ShoppingCartPageComponent},
+    {path:'login', component:LoginComponent}
 ];
