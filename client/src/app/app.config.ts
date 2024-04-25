3d22262ac269f72d6c3d2ba853a86f5728f8f552
Askar Oralkhan <askar4ik123@gmail.com>
2024-04-25 22:37:20 +0500

    created interceptor to add bearer token to request headers

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -2,8 +2,15 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),provideHttpClient()],
+  providers: [provideRouter(routes), 
+    provideHttpClient(withInterceptorsFromDi()), {
+      provide:HTTP_INTERCEPTORS,
+      useClass:JwtInterceptor,
+      multi:true
+    }
+  ]
 };
